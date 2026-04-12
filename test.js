89f34cfa30f5ec3a88461a7978255cb9b7bafd44
Kate <radchikovakate@yandex.ru>
2026-04-12 17:46:55 +0300

    feat: create personal data block

--- test.js
+++ test.js
@@ -1,27 +1,53 @@
+
+/* Элементы управления  */
 const finish_button=document.querySelector(".final");
 const control_buttons=document.querySelectorAll(".result_button");
 
+/* Результаты, которые мы собираем  */
+let student_id;
+let student_sex;
+let student_age;
+let student_class;
+let test1ball = [];
 
+let resultPersonalforText = "";
 let result1forText="";
 
+/* Константы для выдачи интерпритации  */
 
-function isEverythingMarked(){
 
 
-const identityQuestion = document.querySelectorAll(".identity_question");
+/* Функции проверяющие заполнение и подсвечивающие незаполненные вопросы */
 
+function checkPersonalFields() {
+    const fields = document.querySelectorAll('.personal_select');
+    let checked = true;
+
+    fields.forEach(field => {
+    if (field.value === "") {
+        field.style.border = "2px solid red"; // Подсвечиваем пустые поля
+        checked = false;
+    } 
+    else {
+        field.style.border = ""; // Убираем рамку, если поля заполнили
+    }
+    });
+
+    return checked
+}
+
+function checkIdentityFields(){
+    const identityQuestion = document.querySelectorAll(".identity_question");
     let checked = true;
 
     identityQuestion.forEach((questionBlock) => {
-        
         const checkedRadio = questionBlock.querySelector('input[type="radio"]:checked');
-        
-        if(checkedRadio == null){
-            questionBlock.classList.add("error");
+        if(checkedRadio == null) {
+            questionBlock.classList.add("error");// Подсвечиваем неотвеченные вопросы
             checked = false;
         }
-        else{
-            questionBlock.classList.remove("error");
+        else {
+            questionBlock.classList.remove("error"); // Убираем подсветку, если поля заполнили
         }
     });
 
@@ -30,31 +56,60 @@ const identityQuestion = document.querySelectorAll(".identity_question");
 
 
 
-function identityResults(){
 
-if(isEverythingMarked()){
-    const answers_identity=[];
 
-    for (let i=1; i<= 16; i++){
-        const val = document.querySelector(`input[name="identity${i}"]:checked`).value;
-        answers_identity.push(Number(val));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Функции сбора и обработки информации */
+
+
+function personalResults(){
+
+    if(checkPersonalFields()){
+        const fields = document.querySelectorAll('.personal_select');
+        student_id = fields[0].value;
+        student_sex = fields[1].value;
+        student_age = Number(fields[2].value);
+        student_class = Number(fields[3].value);
+        const text_sex = "Мужской";
+        if(student_sex == "female"){
+            text_sex = "Женский";
+        }
+        resultPersonalforText += `<br> Участник с id: ${student_id}`;
+        resultPersonalforText += `<br> Пол участника: ${text_sex}`;
+        resultPersonalforText += `<br> Возраст, лет: ${student_age}`;
+        resultPersonalforText += `<br> Класс обучения: ${student_class}`;
     }
-
-
-    result1forText += "<br> Опросник Индекс устойчивости идентичности";
-    result1forText += `<br> Шкала самооценки: ${answers_identity[0]+(6-answers_identity[1])+(6-answers_identity[2])+(6-answers_identity[3])}`;
-    result1forText += `<br> Шкала самоэффективности: ${answers_identity[4]+answers_identity[5]+answers_identity[6]+answers_identity[7]}`;
-    result1forText += `<br> Шкала целостности: ${answers_identity[8]+answers_identity[9]+answers_identity[10]+answers_identity[11]}`;
-    result1forText += `<br> Шкала уникальности: ${answers_identity[12]+(6-answers_identity[13])+answers_identity[14]+answers_identity[15]}`;
-
-
-return answers_identity;
-
-
-
 }
 
 
+
+function identityResults(){
+
+    if(checkIdentityFields()){
+        for (let i=1; i<= 16; i++){
+            const val = document.querySelector(`input[name="identity${i}"]:checked`).value;
+            test1ball.push(Number(val));
+        }
+        result1forText += "<br> Опросник Индекс устойчивости идентичности";
+        result1forText += `<br> Шкала самооценки: ${test1ball[0] + (6 - test1ball[1]) + (6 - test1ball[2]) + (6 - test1ball[3])}`;
+        result1forText += `<br> Шкала самоэффективности: ${test1ball[4] + test1ball[5] + test1ball[6] + test1ball[7]}`;
+        result1forText += `<br> Шкала целостности: ${test1ball[8] + test1ball[9] + test1ball[10] + test1ball[11]}`;
+        result1forText += `<br> Шкала уникальности: ${test1ball[12] + (6 - test1ball[13]) + test1ball[14] + test1ball[15]}`;
+    }
 }
 
 
@@ -67,20 +122,28 @@ return answers_identity;
 
 
 finish_button.addEventListener("click", ()=>{
-if(isEverythingMarked()){
- const overlay = document.getElementById('result-screen');
-    const container = document.getElementById('results-data');
-    
-    // Блокируем скролл основной страницы
-    document.body.classList.add('no-scroll');
-    identityResults();
-    // Генерируем много текста для проверки прокрутки
-    let content = "";
-    content += result1forText;
-    container.innerHTML = content;
-    container.style.display = 'none';
-    overlay.style.display = 'flex';
-}
+    //Помечаем красным невыполненные задания
+    const isPersonal = checkPersonalFields();
+    const isIdentity = checkIdentityFields();
+ 
+    // Проверяем все ли блоки заполнены
+    if(isIdentity && isPersonal){
+        const overlay = document.getElementById('result-screen');
+        const container = document.getElementById('results-data');
+        // Блокируем скролл основной страницы
+        document.body.classList.add('no-scroll');
+        // Вычисляем результаты и пишем интерпретацию
+        personalResults()
+        identityResults();
+        // Готовим результат на странице, но скрываем его
+        let content = "";
+        content += resultPersonalforText;
+        content += "<br><br> Результаты <br>"
+        content += result1forText;
+        container.innerHTML = content;
+        container.style.display = 'none';
+        overlay.style.display = 'flex';
+    }
 })
 
 
@@ -89,25 +152,22 @@ control_buttons[0].addEventListener("click", ()=>{
 })
 
 control_buttons[1].addEventListener("click", ()=>{
+    //Показываем результат на странице
     const container = document.getElementById('results-data');
     container.style.display = 'block';
-let data = result1forText.replaceAll(`<br>`, `\n`);
+    //И готовим файл с результатами для скачивания
+    let data = "РЕЗУЛЬТАТЫ: \n";
+    data += resultPersonalforText.replaceAll(`<br>`, `\n`);
+    data += result1forText.replaceAll(`<br>`, `\n`);
 
-
-const blob = new Blob([data], {type: "text/plain"});
-const url=window.URL.createObjectURL(blob);
-
-const a =document.createElement("a");
-a.style.display="none";
-a.href = url;
-a.download = "results.txt";
-
-document.body.appendChild(a);
-a.click();
-
-window.URL.revokeObjectURL(url);
-document.body. removeChild(a);
-
-
-
+    const blob = new Blob([data], {type: "text/plain"});
+    const url=window.URL.createObjectURL(blob);
+    const a =document.createElement("a");
+    a.style.display="none";
+    a.href = url;
+    a.download = `results_${student_id}.txt`;
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    document.body. removeChild(a);
 })
